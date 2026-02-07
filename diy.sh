0055bae8b7b45c355396399f222baad1b7e90185
jasonyunelson-code <jason.yun.elson@gmail.com>
2026-02-08 00:23:17 +0800

    Update diy.sh
    YFYPWrt

--- diy.sh
+++ diy.sh
@@ -7,8 +7,8 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/TikTok YiFanYouPinWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/TikTok YiFanYouPinWRT/g' package/base-files/files/bin/config_generate
 
 # 添加额外的软件包，echo 方式和git clone 方式二选一即可
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
